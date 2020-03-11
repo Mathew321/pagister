5229c3bffdc5c0dc9f4cab4e2b75c9bb4431b86f
Mathew321 <mathew.fedasenka@gmail.com>
2020-03-11 18:27:55 +0100

    re-named HelloWorld component. MInor fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/rooster.png">
+    <LoginPage msg="Welcome Matvei and Marcin!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/LoginPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
 }
 </script>
