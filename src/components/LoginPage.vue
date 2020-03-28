43027f03ab352ced9afd328c9c0784f55c5d0fbd
ms1903 <msamborski724@gmail.com>
2020-03-28 13:17:48 +0100

    De inlog pagina ziet er niet meer uit alsof in de jaren 90.

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,24 +1,33 @@
 <template>
-  <div>
-
-    <img alt="Vue logo" src="../assets/geenMagisterlogo.png" width="500">
+  <div id="heelsite">
+    <link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet">
+    <div id="pagisterlogo" >
+    <img alt="Vue logo" src="../assets/geenMagisterlogo.png" width="150">
+    </div>
         <br>
     <br>
     <br>
     <br>
-
-    <h1>
-      Log in
+    <br>
+    <div id="alles">
+    <div id="loginteksten">
+    <h1 id="welkomtekst">
+      Welkom,
     </h1>
+    <h2 id="logintekst">
+      Log in
+    </h2>
+    </div>
 
     <div class="data">
       <form>
-        <label for="uname"><b>Username </b></label>
-        <input v-model="uname" type="text" placeholder="Enter Username" name="uname" required>
+        <div id="loginpagina">
+        <label for="uname"><b></b></label>
+        <input v-model="uname" type="text" placeholder="Gebruikersnaam" name="uname" required>
           <br>
           <br>
-        <label for="pwd"><b>Password </b></label>
-        <input v-model="password" type="password" placeholder="Enter Password" name="pwd" required>
+        <label for="pwd"><b></b></label>
+        <input v-model="password" type="password" placeholder="Wachtwoord" name="pwd" required>
           <br>
           <br>
         <label>
@@ -26,7 +35,9 @@
         </label>
           <br><br>
         <input v-on:click="login(uname, password)" type="submit" value="Login" />
+        </div>
       </form>
+    </div>
     </div>
   </div>
 </template>
@@ -54,3 +65,80 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+    #loginpagina{
+      
+      font-family: 'Sen', sans-serif;
+      
+     
+    
+      
+    }
+    #loginteksten #welkomtekst{
+      
+      font-family: 'Sen', sans-serif;
+      font-style: bold;
+      font-size: 45px;
+      color: darkolivegreen;
+      
+      
+    }
+      #loginteksten #logintekst{
+      
+      font-family: 'Sen', sans-serif;
+      font-style: bold;
+      font-size: 25px;
+      color: #333;
+
+      
+    }
+    #pagisterlogo{
+      font-family: 'Sen', sans-serif;
+      text-align: left;
+      margin-left: 15px;
+
+        
+
+    }
+
+   
+
+    input[type=text], select {
+  width: 300px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+    input[type=password], select {
+  width: 300px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 300px;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+
+
+</style>
